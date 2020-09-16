25bdec9b677ad9747cb36d738b3690f456aef051
theheisenberg10 <yash.bhirud7@gmail.com>
2020-09-16 11:48:57 +0530

    yash's commit

--- src/main/java/com/Movie_Mania/Project/MovieManiaApplication.java
+++ src/main/java/com/Movie_Mania/Project/MovieManiaApplication.java
@@ -42,6 +42,7 @@ public class MovieManiaApplication implements CommandLineRunner {
 		
 		SpringApplication.run(MovieManiaApplication.class, args);
 		System.out.println("lets start");
+		System.out.println("yash");
 		
 	}
 
