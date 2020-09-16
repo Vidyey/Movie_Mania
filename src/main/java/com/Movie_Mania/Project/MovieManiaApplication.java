cfec1d35b00f2043e6f9b495a0c4fa7e6d4ba696
theheisenberg10 <yash.bhirud7@gmail.com>
2020-09-16 14:47:11 +0530

    application.prop added

--- src/main/java/com/Movie_Mania/Project/MovieManiaApplication.java
+++ src/main/java/com/Movie_Mania/Project/MovieManiaApplication.java
@@ -49,23 +49,31 @@ public class MovieManiaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-	/*	
 		
+		/*
 		String[] stringArray = {"t","s"};
 		
-		Movie m1=new Movie(1, 1, "Ragnarok", "", "", 50, stringArray, LocalDate.now());
+		Movie m1=new Movie(1, 11, "Ragnarok", "", "", 50, stringArray, LocalDate.now());
 		
 		
-		Movie m2=new Movie(2, 2, "Orphan", "", "", 60, stringArray, LocalDate.now());
-		mrepo.save(m1);
-		mrepo.save(m2);
+		Movie m2=new Movie(2, 22, "Orphan", "", "", 60, stringArray, LocalDate.now());
+		
 		
 		
 		List<Movie> movielist1 =new ArrayList<Movie>();
 		movielist1.add(m1);
+	
 		
-		List<Movie> movielist2 =new ArrayList<Movie>();
-		movielist2.add(m2);
+		
+		
+		
+		
+		
+		
+		Theater t1 = new Theater(101, "PVR", "Pune", movielist1, null, "", "");
+		
+		
+		trepo.save(t1);
 		
 		
 		
@@ -73,63 +81,38 @@ public class MovieManiaApplication implements CommandLineRunner {
 		long milli = 123456789999l;
 		java.sql.Time time = new java.sql.Time(milli);
 		
-		Show s1 = new Show(201,time, time, null, "Alpha", m1, 501, 101);     //Theatre 1 Screen 1 show 1
-		Show s2 = new Show(202,time, time, null, "Beta", m2, 502, 101);      //Theatre 1 Screen 2 show 1
-		Show s3 = new Show(203,time, time, null, "Gamma", m1, 503, 102);     //Theatre 2 Screen 1 show 1
-		Show s4 = new Show(204,time, time, null, "Theta", m2, 504, 102);     //Theatre 2 Screen 2 show 1
-			
+		Show s1 = new Show(null,time, time, null, "Alpha", m1, 501, 101);     //Theatre 1 Screen 1 show 1
+		Show s2 = new Show(null,time, time, null, "Alpha", m1, 501, 101);
 		
-		 srepo.save(s1);  srepo.save(s2);  srepo.save(s3);  srepo.save(s4);
+		 
 		 		
 		List<Show> showlist1=new ArrayList<Show>();
 		showlist1.add(s1);
 	
-		List<Show> showlist2=new ArrayList<Show>();
-		showlist2.add(s2);
 		
-		List<Show> showlist3=new ArrayList<Show>();
-		showlist3.add(s3);
-	
-		List<Show> showlist4=new ArrayList<Show>();
-		showlist4.add(s4);
-				
 		
 	
-		Screen sc1=new Screen(501, 101, "A", null, LocalDate.now(),10 , 20);
+		Screen sc1=new Screen(501, 101, "A", showlist1, LocalDate.now(),10 , 20);
 		
-		Screen sc2=new Screen(502,101,"B", null,LocalDate.now(),20,30);
 		
-		Screen sc3=new Screen(503, 102, "C", null, LocalDate.now(),10 , 20);
 		
-		Screen sc4=new Screen(504,102,"D", null,LocalDate.now(),20,30);
 		
-		screpo.save(sc1);
-		screpo.save(sc2);
-		screpo.save(sc3);
-		screpo.save(sc4);
 		
 		List<Screen> screenlist1 = new ArrayList<Screen>();
 		screenlist1.add(sc1);
-		screenlist1.add(sc2);
-		
-		List<Screen> screenlist2 = new ArrayList<Screen>();
-		screenlist2.add(sc3);
-		screenlist2.add(sc4);
 		
 		
 		
 		
-	
 		
 		
-		Theater t1 = new Theater(101, "PVR", "Pune", null, null, "", "");
 		
-		Theater t2 = new Theater(102, "INOX", "Pune", null, null, "", "");
+	*/
 		
-		trepo.save(t1);
-		trepo.save(t2);
 		
-		*/
+		
+		
+		
 	}
 
 }
